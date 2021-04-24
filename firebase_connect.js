c83a83f7fa3d187cd323f6e31fff449f74cf18d1
prabhav-pandya <business.prabhav@gmail.com>
2021-04-24 13:50:44 +0530

    lots of stuff done

---
+++ firebase_connect.js
@@ -0,0 +1,22 @@
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCeMfz94dEYhB0I1oEfruruzD2M9R9Tmwc",
+    authDomain: "upgain-3d715.firebaseapp.com",
+    projectId: "upgain-3d715",
+    storageBucket: "upgain-3d715.appspot.com",
+    messagingSenderId: "294818537237",
+    appId: "1:294818537237:web:a5934ed08dfebc882c931c",
+    measurementId: "G-X9FT8JS67C"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
--- index.js
+++ index.js
@@ -6,9 +6,38 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use("views", express.static(path.join(__dirname, "views")));
 
+
+// Static Middleware
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+/* Pages */
+
+//Home page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/main.html"));
+  });
+
+//blog page
+app.get("/blog", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/blog.html"));
+  });
+
+//Profile page
+app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/profile.html"));
+  });
+
+//Upload page
+app.get("/upload", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/upload.html"));
+  });
+
+//Post page
+app.get("/post", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/post.html"));
   });
 
 app.listen(PORT, () => {
